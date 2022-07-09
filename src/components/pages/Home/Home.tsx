feb3c40ec2c9d6b76aae6b79b7f4d9813d0df83d
ishaban <ishaban.flash@gmail.com>
2022-07-09 19:31:04 +0400

    add home page layout

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import { useStore } from 'effector-react'
 import { NextPage } from 'next'
 import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
 
 import { $bookSeries } from '../../../models/bookSeries'
 import { $contacts } from '../../../models/contacts'
 import { $feedbacks } from '../../../models/feedback'
 import { $news } from '../../../models/news'
+import { pagesPath, staticPath } from '../../../utils/$path'
 import { PageLayout } from '../../PageLayout/PageLayout'
 import { Text } from '../../Text/Text'
 
@@ -15,21 +17,61 @@ export interface Props {}
 
 export const Home: NextPage<Props> = (props) => {
     const bookSeries = useStore($bookSeries)
-    const contacts = useStore($contacts)
+    const { facebook, instagram, twitter, telegram, vkontakte, catchphrase } = useStore($contacts)
     const feedbacks = useStore($feedbacks)
     const news = useStore($news)
 
     return (
         <PageLayout seoTitle="Книжная лавка Тумас" seoDescription="Home" className={styles.base}>
-            <ul>
-                {bookSeries.map(({ id, title, rating }) => (
-                    <li key={id}>
-                        <Text>
-                            {title} / {rating}
-                        </Text>
-                    </li>
-                ))}
-            </ul>
+            <Navbar bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand href={pagesPath.$url().pathname}>
+                        <img
+                            src={staticPath.images.logo_svg}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                            alt="logo"
+                        />{' '}
+                        Книжная лавка Тумас
+                    </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href={pagesPath.$url().pathname}>Книги</Nav.Link>
+                        <Nav.Link href={pagesPath.news.$url().pathname}>Новости</Nav.Link>
+                        <Nav.Link href={pagesPath.$url().pathname}>Отзывы</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href={instagram}>
+                            <i className="bi bi-instagram" />
+                        </Nav.Link>
+                        <Nav.Link href={facebook}>
+                            <i className="bi bi-facebook" />
+                        </Nav.Link>
+                        <Nav.Link href={telegram}>
+                            <i className="bi bi-telegram" />
+                        </Nav.Link>
+                        <Nav.Link href={twitter}>
+                            <i className="bi bi-twitter" />
+                        </Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <img
+                className={styles.headerBackground}
+                src={staticPath.images.home_header_background_jpg}
+                alt="header-background"
+            />
+            <Container>
+                <ul>
+                    {bookSeries.map(({ id, title, rating }) => (
+                        <li key={id}>
+                            <Text>
+                                {title} / {rating}
+                            </Text>
+                        </li>
+                    ))}
+                </ul>
+            </Container>
         </PageLayout>
     )
 }
--- src/utils/$path.ts
+++ src/utils/$path.ts
@@ -16,7 +16,11 @@ export const pagesPath = {
 export type PagesPath = typeof pagesPath
 
 export const staticPath = {
-  favicon_ico: '/favicon.ico'
+  favicon_ico: '/favicon.ico',
+  images: {
+    home_header_background_jpg: '/images/home-header-background.jpg',
+    logo_svg: '/images/logo.svg'
+  }
 } as const
 
 export type StaticPath = typeof staticPath
